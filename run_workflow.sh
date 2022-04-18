44c602f8a8e1ee228f73803a1038f3787f773c4b
Matthew Neeley <u0854535@notchpeak1.int.chpc.utah.edu>
2022-04-18 13:08:05 -0600

    conversion to cram is more robust and cram to storage over rclone is functional.

---
+++ run_workflow.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# run_workflow.sh - bash script to execute snakemake workflow on cluster.
+# The "--jobs 20" argument instructs snakemake to submit at most 20 concurrent
+# jobs to the cluster.
+# The "--latency-wait 30" argument tells snakemake to wait up to 30 seconds
+# after a job completes for the output files to become available.
+
+module load snakemake/6.4.1
+snakemake --cluster-config config.yaml \
+	--jobs 20 \
+	--cluster "sbatch -M {cluster.cluster} -A {cluster.account} -p {cluster.partition}" \
+	--latency-wait 45 
